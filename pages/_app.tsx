10254fa4a4df257caddbe5213d01c76a4c483829
michaelhaggren <michael.haggren@gmail.com>
2022-04-24 18:18:18 +0200

    scroll to top fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,52 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '../components/Layout/Layout';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
-import { Box, ChakraProvider, useColorModeValue } from '@chakra-ui/react';
+import { ChakraProvider as Chakra } from '@chakra-ui/react';
 import { ToastContainer } from 'react-toastify';
 
+if (typeof window !== 'undefined') {
+  window.history.scrollRestoration = 'manual';
+}
 function MyApp({ Component, pageProps, router }: AppProps) {
   const route = useRouter();
   return (
-    <ChakraProvider>
-      <motion.div
-        key={router.route}
-        initial='pageInitial'
-        animate='pageAnimate'
-        transition={{ type: 'spring', duration: 1 }}
-        variants={{
-          pageInitial: {
-            x: 200,
-          },
-          pageAnimate: {
-            x: 0,
-          },
+    <Chakra>
+      <AnimatePresence
+        exitBeforeEnter
+        initial={true}
+        onExitComplete={() => {
+          if (typeof window !== 'undefined') {
+            window.scrollTo({ top: 0 });
+          }
         }}
       >
-        {route.pathname !== '/404' ? (
-          <Layout>
-            <Component {...pageProps} key={router.route} />
-          </Layout>
-        ) : (
-          <Component {...pageProps} />
-        )}
-        <ToastContainer theme='dark' />
-      </motion.div>
-    </ChakraProvider>
+        <motion.div
+          key={router.route}
+          initial='pageInitial'
+          animate='pageAnimate'
+          transition={{ type: 'spring', duration: 0.8 }}
+          variants={{
+            pageInitial: {
+              x: 75,
+            },
+            pageAnimate: {
+              x: 0,
+            },
+          }}
+        >
+          {route.pathname !== '/404' ? (
+            <Layout>
+              <Component {...pageProps} key={router.route} />
+            </Layout>
+          ) : (
+            <Component {...pageProps} />
+          )}
+          <ToastContainer theme='dark' />
+        </motion.div>
+      </AnimatePresence>
+    </Chakra>
   );
 }
 
