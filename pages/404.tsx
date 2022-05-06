92cb89beeb0246add9317e23b7b4a81ce0666325
michaelhaggren <michael.haggren@gmail.com>
2022-05-06 20:54:43 +0200

    404 bg change

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,24 +4,24 @@ import Link from 'next/link';
 function NotFoundPage() {
   return (
     <>
-      <section className="grid h-screen bg-red">
-        <div className="flex justify-center items-center">
-          <button className="border-2 bg-white hover:scale-110 rounded text-black w-40 text-center ">
-            <Link href="/" passHref>
-              <h1 className="uppercas">Let me help you</h1>
+      <section className='grid h-screen'>
+        <div className='flex justify-center items-center'>
+          <button className='border-2 bg-white hover:scale-110 rounded text-black w-40 text-center '>
+            <Link href='/' passHref>
+              <h1 className='uppercas'>Let me help you</h1>
             </Link>
           </button>
         </div>
-        <div className="flex justify-center text-white flex-col items-center">
-          <h1 className="lowercase text-2xl py-6">
+        <div className='flex justify-center text-white flex-col items-center'>
+          <h1 className='lowercase text-2xl py-6'>
             Are you a little lost maybe?
           </h1>
           <Image
             src={PulpFiction}
             width={800}
             height={450}
-            className="rounded-lg"
-            alt="pulp-meme"
+            className='rounded-lg'
+            alt='pulp-meme'
           />
         </div>
       </section>
