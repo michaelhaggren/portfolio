9eb8537fb5061469fca924fb674e3ad5d45e69ed
michaelhaggren <michael.haggren@gmail.com>
2023-11-18 20:42:16 +0100

    update some format

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,25 +22,25 @@ const Home = () => (
   <Box>
     <Head>
       <title> Michael Haggren - Portfolio</title>
-      <meta name="twitter:title" />
-      <meta property="og:title" />
+      <meta name='twitter:title' />
+      <meta property='og:title' />
     </Head>
 
-    <div className="grid place-items-center grid-cols-1">
-      <div className="w-2/4 md:w-1/4">
-        <Section delay="0.2">
-          <Box borderRadius="xl" mb={6} mt={10} p={3} textAlign="center">
+    <div className='grid place-items-center grid-cols-1'>
+      <div className='w-2/4 md:w-1/4'>
+        <Section delay='0.2'>
+          <Box borderRadius='xl' mb={6} mt={10} p={3} textAlign='center'>
             Backend utvecklare på
             <Box
-              className="inline"
+              className='inline'
               color={useColorModeValue('#ffb612', '#ffb612')}
             >
-              <span className="tracking-widest font-semibold">
+              <span className='tracking-widest font-semibold'>
                 <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.avanade.com/se-no"
-                  className="hover:border-b-2 hover:border-orange-orangePrimary "
+                  target='_blank'
+                  rel='noreferrer'
+                  href='https://www.avanade.com/se-no'
+                  className='hover:border-b-2 hover:border-orange-orangePrimary '
                 >
                   {' '}
                   Avanade{' '}
@@ -48,33 +48,33 @@ const Home = () => (
               </span>
             </Box>
             med rötterna från{' '}
-            <span className="tracking-widest   font-semibold">
+            <span className='tracking-widest   font-semibold'>
               Falkenberg.
             </span>{' '}
             För närvarande bosatt i{' '}
-            <span className="tracking-widest   font-semibold"> Stockholm.</span>
+            <span className='tracking-widest   font-semibold'> Stockholm.</span>
           </Box>
 
           <Box>
-            <Box className="flex sm:flex-row flex-col ">
+            <Box className='flex sm:flex-row flex-col '>
               <Box
                 color={useColorModeValue('#141E27', '#ffb612')}
-                className="flex flex-col md:px-0 px-2"
+                className='flex flex-col md:px-0 px-2'
               >
                 <Heading
-                  className=" tracking-widest flex"
-                  as="h1"
-                  variant="page-title"
+                  className=' tracking-widest flex'
+                  as='h1'
+                  variant='page-title'
                 >
                   michael haggren
                 </Heading>
-                <p className=" opacity-40 tracking-widest">systemutvecklare</p>
+                <p className=' opacity-40 tracking-widest'>systemutvecklare</p>
               </Box>
               <Box>
                 <Avatar
-                  size="2xl"
-                  name="Michael Haggren"
-                  src="/images/profil.png"
+                  size='2xl'
+                  name='Michael Haggren'
+                  src='/images/profil.png'
                 />{' '}
               </Box>
             </Box>
@@ -82,207 +82,197 @@ const Home = () => (
               flexShrink={0}
               mt={{ base: 4, md: 0 }}
               ml={{ md: 6 }}
-              textAlign="center"
+              textAlign='center'
             ></Box>
           </Box>
         </Section>
-        <Section delay="0.4">
-          <Heading as="h3" mt={10} fontSize="1em" className=" opacity-90">
+        <Section delay='0.4'>
+          <Heading as='h3' mt={10} fontSize='1em' className=' opacity-90'>
             Om mig
           </Heading>
 
-          <Box alignItems="center" my={2}>
+          <Box alignItems='center' my={2}>
             <Paragraph>
               Teknikintresserad hallänning som gillar att löpa och vandra i
               Nackareservatet, följa aktiemarknaden och lägger även skrämmande
               mycket tid på att hitta ny musik på Spotify/Youtube.
             </Paragraph>
           </Box>
-          <Box alignItems="center" my={10}>
+          <Box alignItems='center' my={10}>
             <Heading
-              as="h3"
-              fontSize="1em"
-              fontWeight="bold "
-              className="opacity-90"
+              as='h3'
+              fontSize='1em'
+              fontWeight='bold '
+              className='opacity-90'
             >
               Main-stack
             </Heading>
             <Paragraph>
               <SimpleGrid
-                columns={[2]}
-                className="flex  mt-2 justify-around gap-4  items-center"
+                columns={[1, 2]}
+                className='flex mt-2 justify-around gap-4 my-4  items-center'
               >
                 <Badge
-                  border="1px"
-                  py="2"
-                  px="1"
-                  className="border text-center w-auto"
-                  colorScheme="grey"
+                  border='1px'
+                  py='2'
+                  px='1'
+                  className='border text-center w-auto'
+                  colorScheme='grey'
                 >
                   .NET/C#
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
                   MSSQL
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
-                  azure devops
+                  azure
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
                   Git
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
-                  Azure
+                  Bicep
                 </Badge>
 
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
-                  bicep
+                  Azure devops
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
                   YAML
                 </Badge>
               </SimpleGrid>
             </Paragraph>
             <Heading
-              as="h3"
-              fontSize="1em"
+              as='h3'
+              fontSize='1em'
               marginTop={4}
-              fontWeight="bold "
-              className="opacity-90"
+              fontWeight='bold '
+              className='opacity-90'
             >
               Sub-stack
             </Heading>
             <Paragraph>
               <SimpleGrid
                 gap={3}
-                columns={[2]}
-                className="flex  mt-2 justify-around gap-4  items-center"
+                columns={[1, 2]}
+                className='flex  mt-2 justify-around gap-4  items-center'
               >
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
-                  Tailwind
+                  TypeScript
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
                   React/Next
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
-                  Typescript
+                  tailwind
                 </Badge>
                 <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
+                  py='2'
+                  px='1'
+                  border='1px'
+                  className='border text-center'
+                  colorScheme='grey'
                 >
                   JAVASCRIPT
-                </Badge>
-
-                <Badge
-                  py="2"
-                  px="1"
-                  border="1px"
-                  className="border text-center"
-                  colorScheme="grey"
-                >
-                  markdown
                 </Badge>
               </SimpleGrid>
             </Paragraph>
           </Box>
         </Section>
         <Divider />
-        <Section delay="0.6">
-          <Box alignItems="center" my={4}>
-            <NextLink href="/projects" scroll={false} passHref>
+        <Section delay='0.6'>
+          <Box alignItems='center' my={4}>
+            <NextLink href='/projects' scroll={false} passHref>
               <Button
                 rightIcon={<ChevronRightIcon />}
-                variant="outline"
-                className=" text-current  bg-orange-orangePrimary"
+                variant='outline'
+                className=' text-current  bg-orange-orangePrimary'
               >
                 Projekt
               </Button>
             </NextLink>
           </Box>
         </Section>
-        <Divider color="white" mb="6" />
-        <Section delay="0.8">
-          <Heading as="h4" fontSize="1.2em" mb="4" className=" opacity-90">
+        <Divider color='white' mb='6' />
+        <Section delay='0.8'>
+          <Heading as='h4' fontSize='1.2em' mb='4' className=' opacity-90'>
             Intressen
           </Heading>
-          <div className="flex gap-2 justify-between items-start md:flex-wrap flex-none md:flex-row flex-col">
+          <div className='flex gap-2 justify-between items-start md:flex-wrap flex-none md:flex-row flex-col'>
             <Paragraph>
               Löpning{' '}
-              <FaRunning className="inline-block text-orange-orangePrimary" />
+              <FaRunning className='inline-block text-orange-orangePrimary' />
             </Paragraph>
             <Paragraph>
               Aktier{' '}
-              <HiChartBar className="inline-block text-orange-orangePrimary" />
+              <HiChartBar className='inline-block text-orange-orangePrimary' />
             </Paragraph>
             <Paragraph>
               Mat{' '}
-              <GiFoodTruck className="inline-block text-orange-orangePrimary" />
+              <GiFoodTruck className='inline-block text-orange-orangePrimary' />
             </Paragraph>
             <Paragraph>
               Musik{' '}
-              <BsMusicPlayer className="inline-block text-orange-orangePrimary" />
+              <BsMusicPlayer className='inline-block text-orange-orangePrimary' />
             </Paragraph>
 
             <Paragraph>
               Programmering{' '}
-              <HiOutlineCode className="inline-block text-orange-orangePrimary" />
+              <HiOutlineCode className='inline-block text-orange-orangePrimary' />
             </Paragraph>
           </div>
         </Section>
--- pages/projects/cryptoapp.tsx
+++ pages/projects/cryptoapp.tsx
@@ -16,9 +16,9 @@ import P from '../../components/Paragraph';
 import Section from '../../components/Section';
 
 const Project = () => (
-  <Container h="full" py={8} px={3}>
-    <ArticleLayout title="Cryptoapp">
-      <Section delay="0.2">
+  <Container h='full' py={8} px={3}>
+    <ArticleLayout title='Cryptoapp'>
+      <Section delay='0.2'>
         <Title>Cryptoapp</Title>
         <P>
           En enkel API cryptoapp som skapades för att vidaretuveckla min kunskap
@@ -26,41 +26,41 @@ const Project = () => (
           Next.js fungerar.
         </P>
       </Section>
-      <Section delay="0.3">
-        <UnorderedList className=" font-montserrat  opacity-90" my={4}>
+      <Section delay='0.3'>
+        <UnorderedList className=' font-montserrat  opacity-90' my={4}>
           <ListItem>
             Information om alla kryptovalutor samlade på ett ställe
           </ListItem>
           <ListItem>Sök och filtrera mellan olika kryptovalutor</ListItem>
         </UnorderedList>
       </Section>
-      <Section delay="0.4">
+      <Section delay='0.4'>
         <List ml={4} my={4}>
           <ListItem mb={4}>
-            <Badge size="sm" className="mr-4">
+            <Badge size='sm' className='mr-4'>
               Tech Stack
             </Badge>
             <span>Next.js, Typescript, Tailwind, SCSS </span>
           </ListItem>
 
           <ListItem>
-            <Badge size="sm" className="mr-4">
+            <Badge size='sm' className='mr-4'>
               Source
             </Badge>
             <Link
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/michaelhaggren/cryptoApp"
+              target='_blank'
+              rel='noreferrer'
+              href='https://github.com/michaelhaggren/cryptoApp'
             >
               github.com/michaelhaggren/cryptoApp
-              <ExternalLinkIcon mx="6px" />
+              <ExternalLinkIcon mx='6px' />
             </Link>
           </ListItem>
         </List>
 
         <SimpleGrid columns={1} gap={4}>
-          <WorkImage src="/images/cryptoapp.png" alt="homepage" />
-          <WorkImage src="/images/searchcurrency.png" alt="search-currencies" />
+          <WorkImage src='/images/cryptoapp.png' alt='homepage' />
+          <WorkImage src='/images/searchcurrency.png' alt='search-currencies' />
         </SimpleGrid>
       </Section>
     </ArticleLayout>
